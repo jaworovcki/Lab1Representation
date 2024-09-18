c7644bf8e66514c2660467344fdab71dbe07b276
Andrii <jaworovcki@gmail.com>
2024-09-19 00:47:58 +0300

    Initial

---
+++ yavandriilab1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace yavandriilab1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
